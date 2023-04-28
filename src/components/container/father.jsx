64dca8a18fa8c0856312f318dbf181568c91719b
juanferGG <juanfe15777@gmail.com>
2023-04-28 15:08:02 -0500

    version estable

--- src/components/container/father.jsx
+++ src/components/container/father.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Child from '../pures/Forms/child';
 
 const Father = () => {
+
+    const [name, setName] = useState('Lucho');
+
+    function showMessage(text){
+        alert(`Menssage recived; ${text}`)
+    }
+
+    function updateName(newName){
+        setName(newName)
+    }
+
     return (
-        <div>
-            <Child></Child>
+        <div style={{background: 'tomato', padding: '10px'}}>
+            <Child name={name} send={showMessage} update={updateName}></Child>
         </div>
     );
 }
--- src/components/pures/Forms/child.jsx
+++ src/components/pures/Forms/child.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const Child = ({ name }) => {
+const Child = ({ name, send, update }) => {
+
+    const messageRef = useRef('')
+    const nameRef = useRef('')
+
     function pressButton(){
-        alert('Default Text')
+        const text = messageRef.current.value
+        alert(`The input message ${text}`)
     }
 
     function pressButtonParams(text){
         alert(`El texto es: ${text}`)
     }
+    function cleanInput(){
+        let text = messageRef.current
+        text.value = ''
+    }
+
+    function submitName(e){
+        e.preventDefault()
+        update(nameRef.current.value)
+    }
 
     return (
-        <div>
-            <p onMouseOver={() => console.log('Mouse Hover')}>Child Component</p>
+        <div style={{background: 'cyan', padding: '30px'}}>
+            <p onMouseOver={() => console.log('Mouse Hover')}>{name}</p>
             <button onClick={() => console.log('Button press')}>Boton1</button>
             <button onClick={() => {pressButton()}}>Button 01</button>
             <button onClick={() => {pressButtonParams('hi')}}>Button 02</button>
@@ -20,9 +34,19 @@ const Child = ({ name }) => {
                 onFocus={() => console.log('focus o input/focus')}
                 onChange={(e) => console.log('Input Changed:', e.target.value)}
                 onCopy={() => console.log('Copied text from Input')}
+                ref = {messageRef}
             />
+            <button onClick={() => send(messageRef.current.value)}>Send Menssage</button>
+            <button onClick={() => {cleanInput()}}>Clean Input</button>
+            <div style={{marginTop: '20px'}}>
+                <form onSubmit={submitName}>
+                    <input ref={nameRef} placeholder='New Name' />
+                    <button type='Submit'>Update Name</button>
+                    <h2 style={{fontWeight: 'bold', color: 'grey'}}>update: {name}</h2>
+                </form>
+            </div>
+
         </div>
     );
 }
-
 export default Child;
